9ed0ddf7fe6b3f7cbd81d86ec2d88927fe3e628c
taarrn <thary@mhs.usk.ac.id>
2025-03-11 23:13:27 +0700

    Update header.h

--- header.h
+++ header.h
@@ -32,3 +32,33 @@ typedef struct nodeStack{
 typedef struct stack{
     nodeStack* top;
 }Stack;
+
+// fumgsi fungsi queue
+Queue* createQueue();
+bool isEmptyQueue(Queue* newQ);
+Node* createNodeQueue(int nomor, char* name, char* layanann);
+void pilihLayanan(char* layanan);
+void enqueue(Queue* newQ, int nomor, char* name, char* layanann, int Silent);
+void dequeue(Queue* newQ, Stack* newS);
+//void peek(Queue* newQ);
+void displayQueue(Queue* newQ);
+void freeQueue(Queue* newQ);
+
+// fungsi fungsi stack
+Stack* createStack();
+bool isEmptyStack(Stack* newS);
+void push(Stack *newS,  int nomor, char* name, char* layanann);
+void displayStack(Stack* newS);
+void undo(Stack* newS, Queue* newQ);
+void freeStack(Stack* newS);
+
+// Fitur tambahaan
+int isValidName(char name[]);
+void trimLeadingSpaces(char *str);
+int isStringEmptyOrWhitespace(const char *str);
+int tambahantrean(Queue* antrean, int* nomorAntrean);
+void printMenu();
+
+#endif
+
+
